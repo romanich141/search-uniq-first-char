9c045903a9e3d9e4ce6add319b9bc7911159a152
romanich141 <roma_ovcharenko@ukr.net>
2021-09-20 15:39:40 +0300

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 // Import stylesheets
 import './style.css';
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+const string = 'hello my name is Will';
+
+const search = (str = '') => {
+  str = [...str].join(',').replaceAll(' ', '');
+
+  const map = new Map();
+
+  for (const char of str) {
+    if (map.has(char)) {
+      map.set(char, map.get(char) + 1);
+    } else {
+      map.set(char, 1);
+    }
+  }
+
+  for (const char of str) {
+    if (map.get(char) === 1) {
+      return char;
+    }
+  }
+
+  retun - 1;
+};
+
+console.log(search(string));
